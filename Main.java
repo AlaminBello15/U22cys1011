c786abf7066babaf8379f782b2ff15b58fb3a707
AlaminBello15 <157647160+AlaminBello15@users.noreply.github.com>
2024-01-25 13:58:35 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operation");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
